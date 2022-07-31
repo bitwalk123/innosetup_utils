0cadafff3ece3c365e48f36650ba557e053840a6
Fuhito Suguri <bitwalk@gmail.com>
2022-07-31 09:45:56 +0900

    update

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+import glob
+import json
 import os
 import sys
 
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QIcon
+from PySide6.QtCore import Qt, QDir
 from PySide6.QtWidgets import (
     QApplication,
     QFileDialog,
-    QLabel,
-    QLineEdit,
-    QMainWindow,
-    QStyle,
-    QToolBar,
-    QToolButton,
+    QMainWindow, QMessageBox,
 )
 
+from modules.isu_toolbar import ISUToolBar
 
-class ISUtility(QMainWindow):
-    entry_dir: QLineEdit = None
+
+class ISUtil(QMainWindow):
+    conf = None
+
+    toolbar: ISUToolBar = None
 
     def __init__(self):
         super().__init__()
@@ -24,42 +24,119 @@ class ISUtility(QMainWindow):
         self.setWindowTitle('utility for Inno Setup Compiler')
 
     def init_ui(self):
-        toolbar = QToolBar()
-        self.addToolBar(Qt.TopToolBarArea, toolbar)
-        #
-        label_dir = QLabel('base directory')
-        toolbar.addWidget(label_dir)
-        #
-        self.entry_dir = QLineEdit()
-        toolbar.addWidget(self.entry_dir)
-        #
-        button_dir = QToolButton()
-        button_dir.setToolButtonStyle(Qt.ToolButtonIconOnly)
-        button_dir.setIcon(
-            QIcon(self.style().standardIcon(QStyle.SP_DirIcon))
-        )
-        button_dir.clicked.connect(self.button_dir_clicked)
-        toolbar.addWidget(button_dir)
-        #
-        button_play = QToolButton()
-        button_play.setToolButtonStyle(Qt.ToolButtonIconOnly)
-        button_play.setIcon(
-            QIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-        )
-        toolbar.addWidget(button_play)
+        self.toolbar = ISUToolBar()
+        self.toolbar.dirClicked.connect(self.button_dir_clicked)
+        self.toolbar.playClicked.connect(self.button_play_clicked)
+        self.toolbar.fileClicked.connect(self.button_file_clicked)
+        self.addToolBar(Qt.TopToolBarArea, self.toolbar)
 
     def button_dir_clicked(self):
         path_dir = QFileDialog.getExistingDirectory(
-            self,
+            parent=self,
             caption='Select Folder',
-            dir=os.environ["HOME"],
+            dir=os.environ['HOME'],
         )
-        self.entry_dir.setText(path_dir)
+        self.toolbar.set_entry(path_dir)
+
+    def button_file_clicked(self):
+        jsonfile = QFileDialog.getOpenFileName(
+            parent=self,
+            caption='Select JSON file',
+            dir=os.environ['HOME'],
+        )
+        json_open = open(jsonfile[0], 'r')
+        self.conf = json.load(json_open)
+        print(self.conf)
+
+    def button_play_clicked(self):
+        if self.toolbar.get_entry() is None:
+            return
+        dir_top = self.toolbar.get_entry()
+        self.generate_sections(dir_top)
+
+    def generate_sections(self, dir_top):
+        list_output = list()
+        # Setup section
+        self.generate_section_setup(dir_top, list_output)
+        # Files section
+        self.generate_section_files(dir_top, list_output)
+        # Icons
+        self.generate_section_icons(list_output)
+        # Output
+        f = open(
+            os.path.join(
+                os.path.dirname(dir_top),
+                '%s.iss' % self.conf['appname']
+            ),
+            'w', encoding='shift_jis'
+        )
+        for line in list_output:
+            f.write(line + '\n')
+        f.close()
+        # completed message
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText('completed!')
+        msgBox.exec()
+
+    def generate_section_setup(self, dir_top, list_output):
+        list_output.append('[Setup]')
+        list_output.append('AppName=%s' % self.conf['appname'])
+        list_output.append('AppVersion=%s' % self.conf['appver'])
+        list_output.append('WizardStyle=modern')
+        list_output.append(
+            'DefaultDirName={autopf}\\%s' % self.conf['appname']
+        )
+        list_output.append('DefaultGroupName=%s' % self.conf['grpname'])
+        list_output.append('Compression=lzma2')
+        list_output.append('SolidCompression=yes')
+        list_output.append(
+            'SourceDir=Z:%s' % dir_top.replace('/', '\\')
+        )
+        list_output.append(
+            'OutputDir=Z:%s' % os.path.dirname(dir_top).replace('/', '\\')
+        )
+        list_output.append(
+            'OutputBaseFilename=%s_%s_setup_%s' % (
+                self.conf['appname'],
+                self.conf['appver'].replace('.', ''),
+                self.conf['build']
+            )
+        )
+        list_output.append('PrivilegesRequired=lowest')
+
+    def generate_section_files(self, dir_top, list_output):
+        list_output.append('[Files]')
+        files = [
+            p.replace('%s/' % dir_top, '')
+            for p in glob.glob('%s/**' % dir_top, recursive=True)
+            if os.path.isfile(p)
+        ]
+        # ---------------------------------------------------------------------
+        for file in files:
+            file_src = file.replace('/', '\\')
+            dir = os.path.dirname(file)
+            if len(dir) == 0:
+                dir_dst = '{app}'
+            else:
+                dir_dst = '{app}\\%s' % dir.replace('/', '\\')
+
+            line = 'Source: "%s"; DestDir: "%s"' % (file_src, dir_dst)
+            list_output.append(line)
+
+    def generate_section_icons(self, list_output):
+        list_output.append('[Icons]')
+        list_output.append(
+            'Name: "{group}\\%s"; Filename: "{app}\\%s.exe"' % (
+                self.conf['appname'],
+                self.conf['appname']
+            )
+        )
 
 
 def main():
     app = QApplication(sys.argv)
-    win = ISUtility()
+    win = ISUtil()
     win.show()
     sys.exit(app.exec())
 
---
+++ modules/isu_toolbar.py
@@ -0,0 +1,53 @@
+from PySide6.QtCore import Signal, Qt
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import (
+    QLabel,
+    QLineEdit,
+    QStyle,
+    QToolBar,
+    QToolButton,
+)
+
+
+class ISUToolBar(QToolBar):
+    dirClicked = Signal()
+    playClicked = Signal()
+    fileClicked = Signal()
+
+    def __init__(self):
+        super().__init__()
+        label_dir = QLabel('base directory')
+        self.addWidget(label_dir)
+        # Entry
+        self.entry_dir = QLineEdit()
+        self.addWidget(self.entry_dir)
+        # Dir button
+        button_dir = QToolButton()
+        button_dir.setToolButtonStyle(Qt.ToolButtonIconOnly)
+        button_dir.setIcon(
+            QIcon(self.style().standardIcon(QStyle.SP_DirIcon))
+        )
+        button_dir.clicked.connect(self.dirClicked.emit)
+        self.addWidget(button_dir)
+        # Play button
+        button_play = QToolButton()
+        button_play.setToolButtonStyle(Qt.ToolButtonIconOnly)
+        button_play.setIcon(
+            QIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        )
+        button_play.clicked.connect(self.playClicked.emit)
+        self.addWidget(button_play)
+        # File button
+        button_file = QToolButton()
+        button_file.setToolButtonStyle(Qt.ToolButtonIconOnly)
+        button_file.setIcon(
+            QIcon(self.style().standardIcon(QStyle.SP_FileIcon))
+        )
+        button_file.clicked.connect(self.fileClicked.emit)
+        self.addWidget(button_file)
+
+    def get_entry(self) -> str:
+        return self.entry_dir.text()
+
+    def set_entry(self, path: str):
+        self.entry_dir.setText(path)
